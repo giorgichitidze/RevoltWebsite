d4543ede24a213ec4ee092f04da3f98bdf9d420d
chitidzegiorgi2 <chitidzegiorgi2@gmail.com>
2020-09-25 00:51:31 +0400

    Added authentication but still have some issues

--- app.js
+++ app.js
@@ -1,37 +1,42 @@
-var express = require("express");
-var app = express();
-var mongoose = require("mongoose");
-var parser = require("body-parser");
+var express = require("express"),
+  app = express(),
+  mongoose = require("mongoose"),
+  parser = require("body-parser"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  User = require("./models/user");
 const { Db, MongoClient } = require("mongodb");
 const bodyParser = require("body-parser");
+const user = require("./models/user");
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.static("Images"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost:27017/Revolt_Images",{useNewUrlParser:true})
-    .then(()=> console.log("MongoDB Connected"))
-    .catch(err=> console.log("This is error : " + err));
-
-
+mongoose
+  .connect("mongodb://localhost:27017/Revolt_Images", { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("This is error : " + err));
 
 var RevoltAdminsSchema = new mongoose.Schema({
-
-    name:String,
-    email:String,
-    password:String
+  name: String,
+  email: String,
+  password: String,
 });
 //Schema Setup
 var RevoltImagesSchema = new mongoose.Schema({
-
-    name: String,
-    Image: String
-
+  name: String,
+  Image: String,
 });
 
-var Images = mongoose.model("revoltimages",RevoltImagesSchema); 
+var Images = mongoose.model("revoltimages", RevoltImagesSchema);
 //var Admins = mongoose.model("revoltadmins",RevoltAdminsSchema);
 
 // Admins.create({
@@ -51,7 +56,6 @@ var Images = mongoose.model("revoltimages",RevoltImagesSchema);
 
 //        name:"the tesli gogo",
 //        Image: "https://images.unsplash.com/photo-1600353068218-27240d813841?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
-    
 
 //    },function(err,images){
 //        if(err){
@@ -61,63 +65,101 @@ var Images = mongoose.model("revoltimages",RevoltImagesSchema);
 //            console.log(images);
 //        }
 //    });
-app.get("/home",function(req,res){
 
-    res.render("index");
+//PASSPORT CONFIG
+app.use(
+  require("express-session")({
+    secret: "This is admin secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/home", function (req, res) {
+  res.render("index");
 });
 
-app.get("/",function(req,res){
-
-    
-    res.render("index");
-
+app.get("/", function (req, res) {
+  res.render("index");
 });
 
-app.get("/admin",function(req,res){
-    res.render("admin");
+app.get("/admin", function (req, res) {
+  res.render("admin");
 });
 
-app.post("/FelledRoots",function(req,res){
-    
-    Images.create(req.body.Images,function(err,newimage){
-          
-              
-        res.redirect("/FelledRoots");
-          
-    });
+app.post("/FelledRoots", function (req, res) {
+  Images.create(req.body.Images, function (err, newimage) {
+    res.redirect("/FelledRoots");
+  });
 });
 
-app.get("/FelledRoots/:id",function(req,res){
-
-    
-    Images.findById(req.params.id,function(err,foundImg){
-
-        if(err){
-            res.redirect("/FelledRoots");
-        }else{
-            res.render("show",{img:foundImg});
-        }
-
-    });
-
+app.get("/FelledRoots/:id", function (req, res) {
+  Images.findById(req.params.id, function (err, foundImg) {
+    if (err) {
+      res.redirect("/FelledRoots");
+    } else {
+      res.render("show", { img: foundImg });
+    }
+  });
 });
 
-app.get("/admin/panel",function(req,res){
-    res.render("panel");
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
 });
 
-app.get("/FelledRoots",function(req,res){
-
-    Images.find({},function(err,allImages){
-
-        console.log(allImages);
-        res.render("FRMC",{RevoltImages:allImages});
-    });
-    
+app.get("/admin/panel", function (req, res) {
+  console.log(req.currentUser);
+  res.render("panel");
 });
 
+// app.post("/admin/panel", function (req, res) {
+//   var newUser = new User({ username: "test@mail.com" });
+//   User.register(newUser, "12345", function (err, user) {
+//     if (err) {
+//       console.log(
+//         "===============================Error==============================="
+//       );
+//       console.log(err);
+//       console.log(
+//         "===============================End of Error==============================="
+//       );
+//       return res.render("admin");
+//     }
+//     passport.authenticate("local")(req, res, function () {
+//       res.redirect("/panel");
+//     });
+//   });
+// });
 
-app.listen(2000, function(){
-    console.log("Revolt Server Started!!!");
-})
+app.post(
+  "/admin",
+  passport.authenticate("local", {
+    successRedirect: "/admin/panel",
+    failureRedirect: "/admin",
+  }),
+  function (req, res) {}
+);
 
+app.get("/FelledRoots", function (req, res) {
+  Images.find({}, function (err, allImages) {
+    console.log(allImages);
+    res.render("FRMC", { RevoltImages: allImages });
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
+
+app.listen(2000, function () {
+  console.log("Revolt Server Started!!!");
+});
